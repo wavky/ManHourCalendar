374aafd10872bb403593aa307aa3a4f8447f20b8
wavky <228784830@qq.com>
2017-09-09 17:10:05 +0800

    split drawer class into io.py

--- src/main.py
+++ src/main.py
@@ -10,19 +10,19 @@ Example to run the program.
 
 from datetime import date
 
+import src.io as io
 import src.manhour_calendar as mh
 from src.job import Job
 
 date = date.today()
 month = mh.Month(date.year, date.month)
 month.initialize_days()
-days = month.days
 
 job = Job(required_manhour=170, daily_work_hours=7.5, hourly_pay=2000, max_daily_overhours=2)
 schedule = mh.Schedule(job, month)
 schedule.schedule()
 
-mhc = mh.MHCalendarDrawer(width=14)
+mhc = io.MHCalendarDrawer(width=14)
 mhc.draw(schedule)
 
 month.next_day.checkin(7.5)
@@ -32,5 +32,3 @@ month.next_day.checkin()
 schedule.adjust(day_off=[6, 12, -9, -17])
 schedule.schedule()
 mhc.draw(schedule)
-
-# TODO: 控制面板
